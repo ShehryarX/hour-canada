e9681bd198588a7a8634d1a5129d6bf940ab00d6
Shehryar Assad <xshehryar@gmail.com>
2018-08-20 15:29:29 -0400

    popup

--- Hour-Canada/App.js
+++ Hour-Canada/App.js
@@ -3,6 +3,7 @@ import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
 import Login from './screens/Login';
+import Popup from './screens/Popup';
 
 export default class App extends React.Component {
   state = {
@@ -22,8 +23,9 @@ export default class App extends React.Component {
       return (
         <View style={styles.container}>
           {/* {Platform.OS === 'ios' && <StatusBar barStyle="default" />} */}
-          <AppNavigator />
+          {/* <AppNavigator /> */}
           {/* <Login /> */}
+          <Popup />
         </View>
       );
     }
--- Hour-Canada/screens/Popup.js
+++ Hour-Canada/screens/Popup.js
@@ -1,15 +1,85 @@
 import React, { Component } from 'react';
 import { 
   View,
-  Text
+  Text,
+  TouchableOpacity,
+  Image,
+  TextInput
 } from 'react-native';
 
-export default class Popup extends Comment {
+export default class Popup extends Component {
+  state = {
+    title: '',
+    dateAndTime: '',
+    description: '',
+  }
+
   render() {
     return (
-      <View>
-        <Text>Hey</Text>
+      <View styles={styles.containerStyle}>
+        <View style={{backgroundColor: '#F05C4B', paddingTop: 30, paddingBottom: 10}}>
+          <Image source={require('../assets/images/hourglass.png')} style={{height: 60, width: 60, alignSelf: 'center',}} />
+        </View>
+
+        <View style={{flexDirection: 'row', paddingVertical: 5}}>
+          <TouchableOpacity style={styles.buttonStyle1}>
+            <Text style={{fontFamily: 'raleway-bold', fontSize: 24, textAlign: 'center', color: 'white'}}>REQUEST</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonStyle2}>
+            <Text style={{fontFamily: 'raleway-bold', fontSize: 24, textAlign: 'center', color: '#F05C4B'}}>GIVE</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          <Text style={{fontFamily: 'raleway-bold', fontSize: 32, textAlign: 'center', color: '#F05C4B'}}>Hours left: 5 </Text>
+          <View style={{margin: 20}}>
+            <TextInput
+              style={{height: 40, fontSize: 24}}
+              onChangeText={(title) => this.setState({title})}
+              placeholder=' TITLE'
+              value={this.state.title}
+              underlineColorAndroid={'red'}
+            />
+            <TextInput
+              style={{height: 40, fontSize: 24}}
+              onChangeText={(dateAndTime) => this.setState({dateAndTime})}
+              placeholder=' DATE AND TIME'
+              value={this.state.dateAndTime}
+              underlineColorAndroid={'red'}
+            />
+            <TextInput
+              style={{height: 40, fontSize: 24}}
+              onChangeText={(description) => this.setState({description})}
+              placeholder=' DESCRIPTION'
+              value={this.state.description}
+              multiline={true}
+              numberOfLines={10}
+              underlineColorAndroid={'red'}
+              editable={true}
+            />
+          </View>
+        </View>
       </View>
     ) 
+  }
+}
+
+const styles = {
+  containerStyle: {
+    backgroundColor: '#fff',
+  },
+  buttonStyle1: {
+    backgroundColor: '#F05C4B',
+    borderRadius: 5,
+    margin: 10,
+    padding: 10,
+    flex: 1,
+  },
+  buttonStyle2: {
+    backgroundColor: '#F0F0F0',
+    borderRadius: 5,
+    margin: 10,
+    padding: 10,
+    flex: 1,
   }
 }
